60ea36fbeef333f868c14268f8eca18a75ccb484
Obakeng <mosadiobakeng7@gmail.com>
2022-02-25 10:34:43 +0200

    Created a 2d matrix and added initial elements to it

--- visualizer/src/UI/molecules/Grid.tsx
+++ visualizer/src/UI/molecules/Grid.tsx
@@ -5,7 +5,18 @@ function Grid() {
   
   let [nodes, setNode] = useState([]);
 
-  
+  let matrix:number[][] = [];
+
+  for (let i = 0; i < 5; i++) {
+    let elements:number[] = [];
+    for (let j = 0; j < 10; j++) {
+      elements.push(0);
+    }
+
+    matrix.push(elements);
+  }
+
+  console.log(matrix);
 
   return (
     <div>
