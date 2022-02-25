89577dab94794332bead18db93bd98068302edc5
Obakeng <mosadiobakeng7@gmail.com>
2022-02-25 10:54:57 +0200

    Changed display property to block

--- visualizer/src/UI/atoms/Node.tsx
+++ visualizer/src/UI/atoms/Node.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Node() {
   return (
-    <div className='w-8 h-8 border-sky-700 border-2'></div>
+    <div className='w-8 h-8 border-sky-700 border-2 block'></div>
   )
 }
 
