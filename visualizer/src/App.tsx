e9359dda29b97af0a23d12f826f71ade7ae0c864
Obakeng <mosadiobakeng7@gmail.com>
2022-02-25 10:29:14 +0200

    Removed Node reference

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Node from './UI/atoms/Node';
 
 function App() {
 
-  let final:number[] = [];
-
   return (
     <div className="App">
-        <Node/>
       
     </div>
   );
