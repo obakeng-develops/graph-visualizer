db7a1678162a65bcee9c8293f91e18b727e2717c
Obakeng <mosadiobakeng7@gmail.com>
2022-03-03 10:03:01 +0200

    Changed number of rows

--- visualizer/src/UI/molecules/Grid.tsx
+++ visualizer/src/UI/molecules/Grid.tsx
@@ -5,7 +5,7 @@ function Grid() {
 
   let matrix:number[][] = [];
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     let elements:number[] = [];
     for (let j = 0; j < 5; j++) {
       elements.push(0);
@@ -14,7 +14,7 @@ function Grid() {
   }
 
   return (
-    <div className='grid grid-cols-5 grid-rows-5 m-0 '>
+    <div className='grid grid-cols-5 grid-rows-5 m-0 absolute'>
       {
         matrix.map((row, rowIdx) => {
           return <>
