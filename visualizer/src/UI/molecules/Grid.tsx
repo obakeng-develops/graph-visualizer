281f7b2a7d0a4f71145edaa8d5440b5298327d04
Obakeng <mosadiobakeng7@gmail.com>
2022-03-03 09:57:31 +0200

    Removed grid properties

--- visualizer/src/UI/molecules/Grid.tsx
+++ visualizer/src/UI/molecules/Grid.tsx
@@ -14,7 +14,7 @@ function Grid() {
   }
 
   return (
-    <div className='flex grid grid-cols-5 absolute m-0'>
+    <div className=''>
       {
         matrix.map((row, rowIdx) => {
           return <>
